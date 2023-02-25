53d027aa4987141c097ccf4be36f3ee45e021f7d
Felipe Parra <fpr29.10@gmail.com>
2023-02-24 22:14:01 -0600

    Add mainStyles.ts with styles for view, h1Title, and buttonPrimary

---
+++ src/styles/mainStyles.ts
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  view: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  h1Title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    color: 'teal',
+  },
+  buttonPrimary: {
+    backgroundColor: 'red',
+    color: 'green',
+    padding: 8,
+    borderRadius: 4,
+    borderColor: '#000',
+    borderWidth: 1,
+  },
+});
