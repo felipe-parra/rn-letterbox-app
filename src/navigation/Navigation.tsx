d501af6f8944bea129d858942a51729d8541b05d
Felipe Parra <fpr29.10@gmail.com>
2023-02-25 20:36:35 -0600

    Add DetailScreen to navigation stack

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -2,8 +2,16 @@ import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import AboutScreen from '../screens/AboutScreen';
+import DetailScreen from '../screens/DetailScreen';
+import {Movie} from '../interfaces/movieInterface';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParams = {
+  Home: undefined;
+  About: undefined;
+  DetailScreen: Movie;
+};
+
+const Stack = createNativeStackNavigator<RootStackParams>();
 
 function App() {
   return (
@@ -24,6 +32,13 @@ function App() {
         component={AboutScreen}
         options={{
           title: 'About',
+        }}
+      />
+      <Stack.Screen
+        name="DetailScreen"
+        component={DetailScreen}
+        options={{
+          title: 'Detail Screen',
         }}
       />
     </Stack.Navigator>
