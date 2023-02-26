8c1f29e3c5688f0c821e28dfeb2fb08c6c63e390
Felipe Parra <fpr29.10@gmail.com>
2023-02-25 20:36:46 -0600

    Commit "Add styling to DetailScreen and implement useMovieDetails hook"

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,10 +1,89 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {StackScreenProps} from '@react-navigation/stack';
+import {RootStackParams} from '../navigation/Navigation';
+import useMovieDetails from '../hooks/useMovieDetails';
+import MovieDetails from '../components/ui/movies/details/MovieDetails';
 
-export default function DetailScreen() {
+const screenHeight = Dimensions.get('screen').height;
+
+interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {}
+
+export default function DetailScreen({route}: Props) {
+  const movie = route.params;
+  const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  const {isLoading, cast, movieFull} = useMovieDetails(movie.id);
   return (
-    <View>
-      <Text>Detail Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.imageContainer}>
+        <View style={styles.imageBorder}>
+          <Image source={{uri}} style={styles.posterImage} />
+        </View>
+      </View>
+      <View style={styles.marginContainer}>
+        <Text style={styles.subTitle}>{movie.original_title}</Text>
+        <Text style={styles.title}>{movie.title}</Text>
+      </View>
+      {isLoading ? (
+        <ActivityIndicator size={35} color="grey" style={{marginTop: 20}} />
+      ) : (
+        <MovieDetails movieFull={movieFull!} cast={cast} />
+      )}
+    </ScrollView>
   );
 }
+const styles = StyleSheet.create({
+  imageContainer: {
+    // backgroundColor: 'red',
+    // overflow: 'hidden',
+    width: '100%',
+    height: screenHeight * 0.7,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.24,
+    shadowRadius: 7,
+
+    elevation: 9,
+    borderBottomEndRadius: 25,
+    borderBottomStartRadius: 25,
+  },
+
+  imageBorder: {
+    flex: 1,
+    overflow: 'hidden',
+    borderBottomEndRadius: 25,
+    borderBottomStartRadius: 25,
+  },
+  posterImage: {
+    flex: 1,
+  },
+
+  marginContainer: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  subTitle: {color: 'black', fontSize: 16, opacity: 0.8},
+  title: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  backButton: {
+    position: 'absolute',
+    zIndex: 999,
+    elevation: 9,
+    top: 30,
+    left: 5,
+  },
+});
